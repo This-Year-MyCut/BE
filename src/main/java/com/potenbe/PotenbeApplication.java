6583af2f57671439db60d56dc15a3e124de3a2c3
ARProxy <hocci0222@kakao.com>
2023-11-08 19:41:24 +0900

    first

---
+++ src/main/java/com/potenbe/PotenbeApplication.java
@@ -0,0 +1,13 @@
+package com.potenbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotenbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotenbeApplication.class, args);
+	}
+
+}
